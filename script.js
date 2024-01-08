3c80c2e531fafceb45db0363cb9a92ddd9594785
DevHredoy <rafiul.hredoy@technonext.com>
2024-01-08 19:55:53 +0600

    updated

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+const fruits=[
+
+{id:0,image:"fruits/grape fruit.webp",title:"grapefruit",price:34},
+{id:1,image:"fruits/Bubblegum.jpeg",title:"bubblegum",price:23,},
+{id:2,image:"fruits/lichu.jpg",title,price},
+{id:3,image:"fruits/Loquat.jpg",title:"Loquat",price:23}
+
+
+
+];
