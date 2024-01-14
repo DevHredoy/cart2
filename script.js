0c5449c7455679bdf1326d099743012cbd771a60
DevHredoy <rafiul.hredoy@technonext.com>
2024-01-14 17:25:25 +0600

    variable

--- script.js
+++ script.js
@@ -9,25 +9,15 @@ const uniqueFruits=[
 
 ];
 
+const myfruits=document.querySelector("#body-contents");
 
-
-
-//lets make basic card component
-
-
-const root=document.querySelector("#root");
-const count=document.querySelector("#count");
-
-const cartItem=document.querySelector("#cartItem");
-const total=document.querySelector("#total");
-//const bodyContents=document.querySelector("#body-contents");
-const bodycontents=document.getElementById("body-contnent").innerHTML ;
-
-
-document.getElementById("body-content").innerHTML = uniqueFruits.map((item)=>
+let i=0;
+myfruits.innerHTML=uniqueFruits.map((item)=>
 {var{
 image,title,price
 }=item;
+
+
 return(
     `<div class='box'>
         <div class='img-box'>
@@ -45,97 +35,5 @@ return(
 }).join('')
 
 
+console.log("myfruits: ",myfruits);
 
-
-
-
-
-
-
-
-
-
-
-
-// bodyContents.innerHTML= uniqueFruits.map((item) => {
-//     var { image, title, price } = item;
-//     return (
-//         `<div class='box'>
-//             <div class='img-box'>
-//                 <img class='images' src=${image}></img>
-//             </div>
-//             <div class='bottom'>
-//                 <p>${title}</p>
-//                 <h2>$ ${price}.00</h2>` +
-//                 "<button onclick='addtocart(" + (i++) + ")'>Add to cart</button>" +
-//                 `</div>
-//         </div>`
-//     );
-// }).join('');
-
-
-// Now you can append bodyContents to the root element
-// root.innerHTML = bodyContents;
-
-// let i=0;
-
-// document.getElementById("root").innerHTML = uniqueFruits.map((item)=>
-// {var{
-// image,title,price
-// }=item;
-// return(
-//     `<div class='box'>
-//         <div class='img-box'>
-//             <img class='images' src=${image}></img>
-//         </div>
-//     <div class='bottom'>
-//     <p>${title}</p>
-//     <h2>$ ${price}.00</h2>`+
-//     "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
-//     `</div>
-//     </div>`
-// )
-
-
-// }).join('')
-
-// var cart=[];
-
-// function addtocart(a){
-//     cart.push({...uniqueFruits[a]});
-//     displaycart();
-// }
-// function delElement(a){
-//     cart.splice(a, 1);
-//     displaycart();
-// }
-
-// function displaycart(){
-//     let j = 0, total=0;
-//     document.getElementById("count").innerHTML=cart.length;
-//     if(cart.length==0){
-//         document.getElementById('cartItem').innerHTML = "Your cart is empty";
-//         document.getElementById("total").innerHTML = "$ "+0+".00";
-//     }
-//     else{
-        
-//         document.getElementById("cartItem").innerHTML = cart.map((items)=>
-
-//         {
-//             var {image, title, price} = items;
-//             total=total+price;
-//             document.getElementById("total").innerHTML = "$ "+total+".00";
-//             return(
-//                 `<div class='cart-item'>
-//                 <div class='row-img'>
-//                     <img class='rowimg' src=${image}>
-//                 </div>
-//                 <p style='font-size:12px;'>${title}</p>
-//                 <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
-//                 "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
-//             );
-//         }).join('');
-//     }
-
-    
-// }
